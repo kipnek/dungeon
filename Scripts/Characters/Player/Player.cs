391ceda7cf1040eeb76e99af19f949441834764a
sigh-gone <al.white.dev@gmail.com>
2024-05-18 08:47:58 -0500

    initial

---
+++ Scripts/Characters/Player/Player.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+    }
+}
+
